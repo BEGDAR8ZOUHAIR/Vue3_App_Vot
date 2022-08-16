af621a408723a3b48bbbe7d0521b0aefcfd6bf26
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2022-08-16 16:18:23 +0100

    first version

---
+++ src/components/AverageRating.vue
@@ -0,0 +1,30 @@
+                                                        
+
+<template>
+    <div class='container my-4'>
+        <div class="d-flex justify-content-between align-items-center">
+            <h5>Reviews: 
+                <span class="badge bg-dark rounded-pill">
+                    {{store.reviewsLength}}
+                </span>
+            </h5>
+            <h5>Average: 
+                <span class="badge bg-warning text-dark rounded-pill">
+                    {{ store.calculateReviewsAverage }}
+                </span>
+            </h5>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import {useReviewStore} from '../stores'
+
+    //get store
+    const store = useReviewStore();
+</script>
+
+<style>
+</style>
+                                                        
+                                                    
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+                                                    
+                                                   
+<template>
+    <div class='navbar navbar-right my-4'>
+        <div class="container d-flex justify-content-center">
+            <router-link to="/" class="navbar-brand text-dark" style="text-decoration: none;color: #000 !important;">
+                <h3>
+                    <i class="bi bi-star-half fs-2 text-danger"></i> Vuejs 3 Review App
+                </h3>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
+                                                    
+                                                
